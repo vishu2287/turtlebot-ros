5bae3c0c844e35bdda99ca51b2a5fdfbaa3c0379
saxena.saurabh90@gmail.com <saxena.saurabh90@gmail.com@e6cfabae-315a-9d14-5fa2-c25fd2024bc5>
2013-04-28 11:08:19 +0000

    Deleting build

--- turtlebot_move/build/devel/setup.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-# generated from catkin/cmake/templates/setup.zsh.in
-
-CATKIN_SHELL=zsh
-emulate sh # emulate POSIX
-. "/home/saurabh/Documents/Thesis/stacks/turtlebot_move/build/devel/setup.sh"
-emulate zsh # back to zsh mode
